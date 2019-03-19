7fc1ff39e270f01d9621531e10a2e8f1685edf1a
Josh Wulf <josh@magikcraft.io>
2019-03-19 16:16:21 +1000

    Update run.sh
    Comment out sma-bootstrap mount. It is built into the image.

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 docker run -it -p 25665:25565 \
     --mount source=scriptcraft-worlds,target=/server/worlds \
     --mount source=scriptcraft-cache,target=/server/cache \
-    --mount type=bind,src=$(pwd)/test-plugin,dst=/server/scriptcraft-plugins/test \
-    --mount type=bind,src=$(pwd)/resources/js/sma-bootstrap,dst=/server/scriptcraft/plugins/sma-bootstrap \
+# The following mount demonstrates how to mount an SMA plugin in the image
+#    --mount type=bind,src=$(pwd)/test-plugin,dst=/server/scriptcraft-plugins/test \
+# Uncomment the line below to test changes to sma-bootstrap without rebuilding the image - for sma-bootstrap dev only    
+#    --mount type=bind,src=$(pwd)/resources/js/sma-bootstrap,dst=/server/scriptcraft/plugins/sma-bootstrap \
     magikcraft/scriptcraft
