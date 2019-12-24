aac360513b6256923a0338f6fc4a0bd02d23bef0
Josh Wulf <josh@magikcraft.io>
2019-12-25 01:30:15 +1300

    1.15

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+MINECRAFT_TAG=1.15
 docker run -it -p 25665:25565 \
     --mount source=scriptcraft-worlds,target=/_server_/worlds \
     --mount source=scriptcraft-cache,target=/_server_/cache \
     --mount type=bind,src=$(pwd)/resources/js/sma-bootstrap,dst=/_server_/scriptcraft/plugins/sma-bootstrap \
     --mount type=bind,src=$(pwd)/test/test-plugin,dst=/_server_/scriptcraft-plugins/test \
     --mount type=bind,src=$(pwd)/test/test-plugin-2,dst=/_server_/scriptcraft-plugins/test-plugin-2 \
-    magikcraft/scriptcraft
+    magikcraft/scriptcraft:$MINECRAFT_TAG
 
 # The following mount demonstrates how to mount an SMA plugin in the image
 #    --mount type=bind,src=$(pwd)/test-plugin,dst=/_server_/scriptcraft-plugins/test \
